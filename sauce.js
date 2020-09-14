503f07e2c5427c21e0c96b5187f7b05a229cdf6a
Jonic Linley <jonic@100yen.co.uk>
2020-09-14 20:15:34 +0100

    Fix initial state cascade wirth appropriately dispatched event

--- sauce.js
+++ sauce.js
@@ -1,151 +1,139 @@
 class SauceComponent {
   constructor(element) {
-    this.element = element;
+    this.element = element
   }
 
   get data() {
-    return this.element.dataset;
+    return this.element.dataset
   }
 
   setState(value) {
-    this.data.sauceState = value;
+    this.data.sauceState = value
   }
 
   unsetState() {
-    delete this.data.sauceState;
+    delete this.data.sauceState
   }
 }
 
 class SauceStateController {
   constructor(controller) {
-    this.components = [];
-    this.controller = controller;
-    this.initialized = false;
-    this.states = [];
+    this.components = []
+    this.controller = controller
+    this.initialized = false
+    this.states = []
 
-    this.initComponents();
-    this.initStates();
-    this.setInitialState();
+    this.initComponents()
+    this.initStates()
+    this.setInitialState()
   }
 
   get componentElements() {
-    if (this.controller.tagName === "STATE-CONTROLLER") {
+    if (this.controller.tagName === 'STATE-CONTROLLER') {
       return Array.from(
         this.controller.parentNode.querySelectorAll(
-          `:scope ~ .${this.controller.getAttribute("for")}`
+          `:scope > .${this.controller.getAttribute('for')}`
         )
-      );
+      )
     }
 
-    return [this.controller];
+    return [this.controller]
   }
 
   get stateElements() {
-    return Array.from(this.controller.querySelectorAll(":scope > state"));
+    return Array.from(this.controller.querySelectorAll(':scope > state'))
   }
 
   initComponents() {
     this.components = this.componentElements.map(
       (element) => new SauceComponent(element)
-    );
+    )
   }
 
   initStates() {
     this.states = this.stateElements.map(
       (element, index) => new SauceState({ controller: this, element, index })
-    );
+    )
   }
 
   cascadeState(fromIndex) {
-    if (!this.initialized) return;
+    if (!this.initialized) return
 
-    this.unsetState();
-    this.invokeCallbackForState(fromIndex - 1);
+    this.unsetState()
+    this.invokeCallbackForState(fromIndex - 1)
   }
 
   invokeCallbackForState(index) {
-    if (index < 0) return;
+    if (index < 0) return
 
-    this.states[index].forceApply();
+    this.states[index].cascadeState()
   }
 
   setInitialState() {
-    const states = Array.from(this.states);
+    const states = Array.from(this.states)
 
     states.reverse().forEach((state) => {
-      if (this.initialized) {
-        return;
-      }
+      if (this.initialized) return
 
-      this.initialized = true;
-      state.forceApply();
-    });
+      this.initialized = true
+      state.cascadeState()
+    })
   }
 
   setState(value) {
-    if (!this.initialized) return;
+    if (!this.initialized) return
 
-    this.components.forEach((component) => component.setState(value));
+    this.components.forEach((component) => component.setState(value))
   }
 
   unsetState() {
-    this.components.forEach((component) => component.unsetState());
+    this.components.forEach((component) => component.unsetState())
   }
 }
 
 class SauceState {
   constructor({ controller, element, index }) {
-    this.controller = controller;
-    this.element = element;
-    this.index = index;
-    this.query = null;
+    this.controller = controller
+    this.element = element
+    this.index = index
+    this.query = null
 
-    this.attach();
-    this.apply();
+    this.addEvent()
   }
 
   get media() {
-    return this.element.getAttribute("media");
+    return this.element.getAttribute('media')
   }
 
   get value() {
-    return this.element.getAttribute("value");
+    return this.element.getAttribute('value')
   }
 
-  attach() {
-    this.query = window.matchMedia(this.media);
-    this.query.addListener((event) => this.callback(event, this));
+  addEvent() {
+    this.query = window.matchMedia(this.media)
+    this.query.addEventListener('change', (event) =>
+      this.applyState(event, this)
+    )
+
+    this.cascadeEvent = new Event('change', {
+      target: this.query,
+    })
   }
 
-  apply(force = false) {
-    const state = this;
-    let event = this.query;
-
-    if (force) {
-      event = { matches: true };
+  applyState(event = this.query) {
+    if (event.target.matches) {
+      this.controller.setState(this.value)
+      return
     }
 
-    this.callback(event, state);
+    this.controller.cascadeState(this.index)
   }
 
-  callback(event, state) {
-    if (event.matches) {
-      state.controller.setState(state.value);
-      return;
-    }
-
-    state.controller.cascadeState(state.index);
-  }
-
-  forceApply() {
-    this.apply(true);
+  cascadeState() {
+    this.query.dispatchEvent(this.cascadeEvent)
   }
 }
 
-const stateControllers = document.querySelectorAll(
-  "[data-sauce-stateful-component], state-controller"
-);
-
-for (const stateController of stateControllers) {
-  new SauceStateController(stateController);
-}
+document
+  .querySelectorAll('[data-sauce-stateful-component], state-controller')
+  .forEach((controller) => new SauceStateController(controller))
